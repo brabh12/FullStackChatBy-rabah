9b2c6dd955ccb503e7b846aeb64a651e6f1a6cc7
Rabah.bct <139927621+brabh12@users.noreply.github.com>
2024-07-16 01:12:38 +0100

    Update chat.jsx

--- Frontend/src/chat.jsx
+++ Frontend/src/chat.jsx
@@ -1,6 +1,6 @@
 import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
 const ChatsPage = (props) => {
-    const chatpr = useMultiChatLogic('a7e9fe42-c543-4383-9fc5-04164bd99f3b', props.user.username, props.user.secret);
+    const chatpr = useMultiChatLogic('/', props.user.username, props.user.secret);
     return (
         <div style={{height: '100vh'}}>
             <MultiChatSocket {...chatpr} />
